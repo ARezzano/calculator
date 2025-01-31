b3d179467f26abd06b8ee1ca87ebb68295289c39
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-01-31 17:59:21 -0300

    Add button elements in HTML file

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ function multiply(val1,val2){
 function divide(val1,val2){
     return val1/val2;
 }
+
+function operate(oper,fNum,sNum){
+    if(oper === "add"){
+        add(fNum,sNum);
+    }else if(oper === "subtract"){
+        subtract(fNum,sNum);
+    }else if(oper === "multiply"){
+        multiply(fNum,sNum);
+    }else if(oper === "divide"){
+        divide(fNum,sNum);
+    }
+}
