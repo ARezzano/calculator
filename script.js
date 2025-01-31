ca58da2c37d2e325aaed0eb0d0b96446154b68f1
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-01-31 16:38:36 -0300

    Create functions for basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+
+function add(val1, val2){
+    return val1 + val2;
+}
+
+function subtract(val1,val2){
+    return val1- val2;
+}
+
+function multiply(val1,val2){
+    return val1*val2;
+}
+
+function divide(val1,val2){
+    return val1/val2;
+}
